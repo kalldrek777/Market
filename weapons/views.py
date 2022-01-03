11de9fcac2bd2990dbcd67f8e4447496fe968fef
kalldrek777 <79919082+kalldrek777@users.noreply.github.com>
2022-01-03 16:51:30 +0300

    Update views.py

--- weapons/views.py
+++ weapons/views.py
@@ -28,22 +28,6 @@ def knife(request):
     return render(request, 'weapons/knifes.html', dict)
 
 
-def weapons(request):
-    qs = Product.objects.all().order_by('-date').filter(category='Оружие')
-    paginator = Paginator(qs, 8)  # Show 8 contacts per page.
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    form = SearchForm()
-    if request.method == 'POST':
-        Search_page(request)
-        page_obj, form = Search_page(request)
-        return render(request, 'Search.html', {'page_obj': page_obj, 'form': form})
-
-    dict = {'page_obj': page_obj, 'form': form}
-
-    return render(request, 'weapons/weapons.html', dict)
 
 
 def wear(request):
