b9f986e0b708e4a30890507811b3b21a263c3080
kalldrek777 <79919082+kalldrek777@users.noreply.github.com>
2022-01-03 16:53:42 +0300

    Update urls.py

--- weapons/urls.py
+++ weapons/urls.py
@@ -5,7 +5,6 @@ from weapons.views import knife, weapons, wear, stickers, accessories
 
 urlpatterns = [
     path('knifes/', knife, name='knife_page'),
-    path('weapons/', weapons, name='weap_page'),
     path('wear/', wear, name='wear'),
     path('stickers/', stickers, name='stickers'),
     path('accessories/', accessories, name='accessories'),
